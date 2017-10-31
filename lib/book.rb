7ce62251b5ad2d63118c6af5ed8ffe2968284418
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-31 08:54:34 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,22 @@
 class Book
+  attr_accessors :title :author :page_count :genre
   def initialize(title)
     @title = title
   end
-  def title
-    @title
-  end
-  def author
-    @author
-  end
-  def author=(author)
-    @author = author
-  end
-  def page_count
-    @page_count
-  end
-  def page_count=(page_count)
-    @page_count = page_count
-  end
+  
+#  def title
+#    @title
+#  end
+#  def author
+#    @author
+#  end
+#  def author=(author)
+#    @author = author
+#  end
+#  def page_count
+#    @page_count
+#  end
+#  def page_count=(page_count)
+#    @page_count = page_count
+#  end
 end
